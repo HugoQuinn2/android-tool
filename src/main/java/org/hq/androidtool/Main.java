f68ac6559ba16aa24f2ffec8fa91a5b093ce24e6
HugoQuinn2 <hugoquinn2@gmail.com>
2024-08-24 20:40:36 -0600

    Add licenses to project

--- src/main/java/org/hq/androidtool/Main.java
+++ src/main/java/org/hq/androidtool/Main.java
@@ -3,6 +3,16 @@ package org.hq.androidtool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+ * Copyright (c) 2024, Victor Hugo Gaspar Quinn
+ *
+ * This code is subject to the terms of the restricted-use license
+ * included in the LICENSE file at the root of this repository.
+ * Unauthorized use, modification, or distribution is prohibited without
+ * express written permission from the author.
+ */
+
+
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(MainApp.class);
 
